d1009819230204f2ec2c360abb2c8c99eb05099b
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-03-27 15:35:14 +0100

    Routings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Box from '@mui/material/Box';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Hello from "./components/Hello";
+import Sidebar from "./components/Sidebar/Sidebar";
+import ROUTES from "./routes/ROUTES";
+import { styled } from "@mui/material/styles";
 function App() {
+  const [open, setOpen] = useState(false);
+  const currenUser = { role: "ADMIN" };
+
+  const drawerWidth = 240;
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    })
+  );
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box  sx={{ display: 'flex' }}>
+      <Sidebar
+        routes={ROUTES}
+        userRole={currenUser.role}
+        open={open}
+        handleDrawerClose={handleDrawerClose}
+        handleDrawerOpen={handleDrawerOpen}
+      />
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+              <Routes>
+
+          <Route exact path="/admin/students" element={<Hello />} />
+          <Route exact path="/courses" element={<Hello />} />
+          <Route exact path="/profile" element={<Hello />} />
+        </Routes>
+        </Box>
+          
+        </Box>
   );
 }
 
