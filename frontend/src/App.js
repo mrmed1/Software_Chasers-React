eb26e37c739b4204b0d7e26e79a87b72bd35db67
Mohamed Habaieb <mhabaieb24@gmail.com>
2023-04-04 23:58:23 +0100

    crud etudiant done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,80 +2,15 @@ import Box from '@mui/material/Box';
 import { useState } from "react";
 import { Route, Routes,redirect } from "react-router-dom";
 import "./App.css";
-import Hello from "./components/Hello";
-import Sidebar from "./components/Sidebar/Sidebar";
-import ROUTES from "./routes/ROUTES";
-import { styled } from "@mui/material/styles";
+
 import ListEtudiant from "./components/CrudEtudiant/ListEtudiant";
-import {connectedUser, isLoggedIn} from "./Service/auth.service";
-import Login from "./components/login/Login";
-import { Navigate } from "react-router-dom";
-import SecureRoute from "./components/SecureRoute/secureRoute";
+
 function App() {
-    const [open, setOpen] = useState(false);
-    const currenUser = {role: "ADMIN"};
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    function handleLogin(isLoggedIn) {
-        setIsLoggedIn(isLoggedIn);
-    }
-    const drawerWidth = 240;
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-    const Main = styled("main", {shouldForwardProp: (prop) => prop !== "open"})(
-        ({theme, open}) => ({
-            flexGrow: 1,
-            padding: theme.spacing(3),
-            transition: theme.transitions.create("margin", {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            marginLeft: `-${drawerWidth}px`,
-            ...(open && {
-                transition: theme.transitions.create("margin", {
-                    easing: theme.transitions.easing.easeOut,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-                marginLeft: 0,
-            }),
-        })
-    );
-    const DrawerHeader = styled("div")(({theme}) => ({
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end",
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-    }));
     return (
-        <>
-            {connectedUser() ?
-                <Box  sx={{ display: 'flex' }}>
-                    <Sidebar
-                        routes={ROUTES}
-                        userRole={currenUser.role}
-                        open={open}
-                        handleDrawerClose={handleDrawerClose}
-                        handleDrawerOpen={handleDrawerOpen}
-                    />
-                    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                        <DrawerHeader />
-                        <Routes>
-                            <Route element={<SecureRoute/>} >
-                                <Route exact path="/students" element={<ListEtudiant/>} />
-                            </Route>
-
-                            <Route exact path="/profile" element={<Hello />} />
-                        </Routes>
-                    </Box>
-
-                </Box> : <Login onLogin={handleLogin} />}
-        </>
+          <div className="App">
+              <ListEtudiant/>
+          </div>
 
     )
 }
