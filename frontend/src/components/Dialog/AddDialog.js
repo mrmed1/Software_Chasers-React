597e52447392007a5d6deb3b48393da9ae10712e
mohamedaminebz <60145124+mohamedaminebz@users.noreply.github.com>
2023-04-05 22:16:39 +0100

    update input error

--- frontend/src/components/Dialog/AddDialog.js
+++ frontend/src/components/Dialog/AddDialog.js
@@ -14,6 +14,7 @@ import {
 
 const AddDialog = ({ open, onClose, onSubmit, title, attributes }) => {
   const today = new Date().toISOString().slice(0, 10);
+  const [touched, setTouched] = useState({});
 
   const initialState = Object.fromEntries(
     attributes.map((attr) => [
@@ -77,7 +78,13 @@ const AddDialog = ({ open, onClose, onSubmit, title, attributes }) => {
                     multiline={attr.multiline}
                     minRows={attr.minRows}
                     maxRows={attr.maxRows}
-                 
+                    error={touched[attr.name] && !values[attr.name]} 
+                    helperText={
+                      touched[attr.name] && !values[attr.name]
+                        ? "This field is required"
+                        : " "
+                    } 
+                    onBlur={() => setTouched((prevTouched) => ({ ...prevTouched, [attr.name]: true }))} 
                   />
                 )}
               </>
