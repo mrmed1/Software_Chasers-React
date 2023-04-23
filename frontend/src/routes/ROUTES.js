d66901bde8f603b6edec13872721c24f2bc99a0f
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-04-23 20:46:18 +0100

    --filters

--- frontend/src/routes/ROUTES.js
+++ frontend/src/routes/ROUTES.js
@@ -1,21 +1,8 @@
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-
-import StudentAccount from "../views/studentsViews/StudentAccount";
-
 import CrudPFA from '../views/teacherViews/CrudPFA';
-import MyPFA from '../views/teacherViews/MyPFA';
-import PickPFAstudent from "../views/studentsViews/PickPFAstudent";
-import PublishedPFa from "../views/adminViews/PublishedPFa";
+
 const ROUTES = [
 
-    {
-        path: "/myPFA",
-        name: "My PFA",
-        icon: <InboxIcon/>,
-        element: <MyPFA />,
-        layout: "/teacher",
-        allowedRoles: ["ADMIN"],
-    },
     
     {
         path: "/PFA",
@@ -25,29 +12,6 @@ const ROUTES = [
         layout: "/teacher",
         allowedRoles: ["ADMIN"],
     },
-    {
-        path: "/profile",
-        name: "Show Profile",
-        icon: <InboxIcon/>,
-        element: <StudentAccount />,
-        layout: "/students",
-        allowedRoles: ["ADMIN"],
-    },
-    {
-        path: "/PFA",
-        name: "Pick PFA",
-        icon: <InboxIcon />,
-        element: <PickPFAstudent />,
-        layout: "/students",
-        allowedRoles: ["ADMIN"],
-      },
-      {
-        path: "/PFA",
-        name: "Validate PFA",
-        icon: <InboxIcon />,
-        element: <PublishedPFa />,
-        layout: "/admin",
-        allowedRoles: ["ADMIN"],
-      },
+   
 ]
 export default ROUTES
