67450959be518561a064762e1eb094c5cebf2f48
GanikasSa <61113449+MELEK-WEB@users.noreply.github.com>
2023-04-23 17:38:08 +0100

    dark_mode-light done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,7 @@ import { connectedUser } from "./Service/auth.service";
 import Login from "./components/login/Login";
 import SecureRoute from "./components/SecureRoute/secureRoute";
 import StudentAccount from "./views/studentsViews/StudentAccount";
-import CrudPFA from "./views/teacherViews/CrudPFA";
-import MyPFA from "./views/teacherViews/MyPFA";
-import PickPFAstudent from "./views/studentsViews/PickPFAstudent";
-import PublishedPFa from "./views/adminViews/PublishedPFa";
+
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -76,10 +73,7 @@ function App() {
                 path="/students/profile"
                 element={<StudentAccount />}
               />
-              <Route exact path="/teacher/PFA" element={<CrudPFA />} />
-              <Route exact path="/teacher/myPFA" element={<MyPFA />} />
-              <Route exact path="/admin/PFA" element={<PublishedPFa />} />
-              <Route exact path="/students/PFA" element={<PickPFAstudent />} />
+             
             </Routes>
           </Box>
         </Box>
--- frontend/src/routes/ROUTES.js
+++ frontend/src/routes/ROUTES.js
@@ -1,53 +1,15 @@
-import InboxIcon from '@mui/icons-material/MoveToInbox';
+import InboxIcon from "@mui/icons-material/MoveToInbox";
 
 import StudentAccount from "../views/studentsViews/StudentAccount";
 
-import CrudPFA from '../views/teacherViews/CrudPFA';
-import MyPFA from '../views/teacherViews/MyPFA';
-import PickPFAstudent from "../views/studentsViews/PickPFAstudent";
-import PublishedPFa from "../views/adminViews/PublishedPFa";
 const ROUTES = [
-
-    {
-        path: "/myPFA",
-        name: "My PFA",
-        icon: <InboxIcon/>,
-        element: <MyPFA />,
-        layout: "/teacher",
-        allowedRoles: ["ADMIN"],
-    },
-    
-    {
-        path: "/PFA",
-        name: "PFA",
-        icon: <InboxIcon/>,
-        element: <CrudPFA />,
-        layout: "/teacher",
-        allowedRoles: ["ADMIN"],
-    },
-    {
-        path: "/profile",
-        name: "Show Profile",
-        icon: <InboxIcon/>,
-        element: <StudentAccount />,
-        layout: "/students",
-        allowedRoles: ["ADMIN"],
-    },
-    {
-        path: "/PFA",
-        name: "Pick PFA",
-        icon: <InboxIcon />,
-        element: <PickPFAstudent />,
-        layout: "/students",
-        allowedRoles: ["ADMIN"],
-      },
-      {
-        path: "/PFA",
-        name: "Validate PFA",
-        icon: <InboxIcon />,
-        element: <PublishedPFa />,
-        layout: "/admin",
-        allowedRoles: ["ADMIN"],
-      },
-]
-export default ROUTES
+  {
+    path: "/profile",
+    name: "Show Profile",
+    icon: <InboxIcon />,
+    element: <StudentAccount />,
+    layout: "/students",
+    allowedRoles: ["ADMIN"],
+  },
+];
+export default ROUTES;
