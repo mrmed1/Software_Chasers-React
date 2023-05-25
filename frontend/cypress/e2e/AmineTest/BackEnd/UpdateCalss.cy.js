1f55c7a4d9cdcc152adae789afeab57a0e701d74
mohamedaminebz <60145124+mohamedaminebz@users.noreply.github.com>
2023-05-25 20:13:24 +0100

    fix confli+update Update Class api

--- frontend/cypress/e2e/AmineTest/BackEnd/UpdateCalss.cy.js
+++ frontend/cypress/e2e/AmineTest/BackEnd/UpdateCalss.cy.js
@@ -44,66 +44,20 @@ describe("Student Api Test", () => {
        
       }); 
 
-/*    it("should update the student class ", () => {
-      const data = {
-        firstname: savedStudent.firstname,
-        lastname: savedStudent.lastname,
-        email: savedStudent.email,
-        login:savedStudent.login,
-        password: savedStudent.password,
-        phone:savedStudent.phone,
-        role: "STUDENT",  
-        dob: savedStudent.dob,
-        isPublic: savedStudent.isPublic,
-        class: savedStudent.class,
-        level: '2',
-      };
- 
-       
-      cy.request({
-        method: "PUT",
-        url: `${Url}/Persons/${StudentId}`,
-        body: data,
-        headers: { 'Authorization': `Bearer ${token}` },
-      }).then((resp) => {
-       
-        expect(resp.status).to.equal(200);
-       console.log("Event", resp.body);
-        expect(resp.body.firstname).to.eq(data.firstname);
-        expect(resp.body.lastname).to.eq(data.lastname);
-        expect(resp.body.email).to.eq(data.email);
-        expect(resp.body.login).to.eq(data.login);
-        expect(resp.body.password).to.eq(data.password);
-        expect(resp.body.phone).to.eq(data.phone);
-        expect(resp.body.role).to.eq(data.role);
-        expect(resp.body.class).to.eq(data.class);
-        expect(resp.body.level).to.eq(data.level);
-     
-        
-        expect(resp.body.isPublic).to.eq(data.isPublic);
-      
-        
- 
-    
 
-      
-         
-      });
-      cy.deletePerson();
-    });
- */
-
-    it("should update the event added ", () => {
+    it("should update the student class", () => {
         const data = {
-            firstname: faker.name.firstName(),
-            lastname: faker.name.lastName(),
-            email: faker.internet.email(),
-            login: faker.phone.imei(),
-            password:  faker.phone.imei(),
-            role: "STUDENT",
-            phone:  faker.phone.imei(),
-            level:'2',
-            class:'ing2'
+          firstname: savedStudent.firstname,
+          lastname: savedStudent.lastname,
+          email: savedStudent.email,
+          login:savedStudent.login,
+          password: savedStudent.password,
+          phone:savedStudent.phone,
+          role: "STUDENT",  
+          dob: savedStudent.dob,
+          isPublic: savedStudent.isPublic,
+          class: savedStudent.class,
+          level: '2',
           }
           
         cy.request({
@@ -115,14 +69,36 @@ describe("Student Api Test", () => {
          
           expect(resp.status).to.equal(200);
         
-           
+            
+        expect(resp.status).to.equal(200);
+  
+         expect(resp.body.firstname).to.eq(data.firstname);
+         expect(resp.body.lastname).to.eq(data.lastname);
+         expect(resp.body.email).to.eq(data.email);
+         expect(resp.body.login).to.eq(data.login);
+         expect(resp.body.password).to.eq(data.password);
+         expect(resp.body.phone).to.eq(data.phone);
+         expect(resp.body.role).to.eq(data.role);
+         expect(resp.body.class).to.eq(data.class);
+         expect(resp.body.level).to.eq(data.level);
+      
+         
+         expect(resp.body.isPublic).to.eq(data.isPublic);
+       
+       
         });
       
        
-      });
-      
-    it("should update the event added ", () => {
        
+      });
+      it("should delete Student", () => {
+        cy.request({
+          method: "DELETE",
+          url: `${Url}/Persons/${StudentId}`,
+          headers: { 'Authorization': `Bearer ${token}` },
+        }).then((resp) => {
+          expect(resp.status).to.equal(200);
+        });
       });
 
 
@@ -130,10 +106,5 @@ describe("Student Api Test", () => {
 
 });
 
-after(() => {
-    // get the login function from commands
-    
-  
-    
-  });
+ 
 });
